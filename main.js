6c8c2f0d5a2d2b0cc8d0417bbd43f2a9c0680d61
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-07-06 00:50:01 +0800

    Added simple app setup.

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log("Running...")
+
+const { app, BrowserWindow } = require('electron');
+
+const createWin = () => {
+    const win = new BrowserWindow({
+        width: 400,
+        height: 500,
+        resizable: false,
+        maximizable: false,
+        frame: true,
+        fullscreenable: false,
+        transparent: false,
+        webPreferences: {
+            contextIsolation: true
+        }
+    })
+    win.setMenu(null);
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWin()
+})
