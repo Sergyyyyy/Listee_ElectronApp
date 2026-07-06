bbc10ed8038b20d14e8768a062882e86e0b096eb
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-07-06 15:31:27 +0800

    Added function addTask() to add a task via swal.

--- script.js
+++ script.js
@@ -5,14 +5,55 @@ const btnDelete = document.querySelector(".btnDelete");
 
 const taskList = document.querySelector(".bottom-content");
 
-btnAdd.addEventListener('click', () => {
+async function addTask() {
+    const taskTitle = await swal.fire({
+        inputLabel: 'Enter the task title',
+        input: 'text',
+        showCancelButton: true
+    })
+
+    if (!taskTitle.isConfirmed) { return; }
+
+
+    const task = {
+        title: taskTitle.value,
+    }
+
+    swal.fire({
+        title: 'New task has been added!',
+        icon: 'success',
+        html: ` <h2>Title: ${task.title}</h2> `
+    })
+
     const newTask = document.createElement('div');
-    newTask.textContent = 'Testing';
     newTask.classList.add('task');
+
+    const newP = document.createElement('p');
+    newP.textContent = task.title;
+
+    newTask.append(newP);
+
+    const newBtnDiv = document.createElement('div');
+    newBtnDiv.classList.add('btns-bottom-container');
+
+    const newEditBtn = document.createElement('button');
+    newEditBtn.classList.add('btnEdit');
+    newEditBtn.textContent = 'edit'
+
+    const newDeleteBtn = document.createElement('button');
+    newDeleteBtn.classList.add('btnDelete');
+    newDeleteBtn.textContent = 'delete'
+
+    newBtnDiv.append(newEditBtn);
+    newBtnDiv.append(newDeleteBtn);
+
+    newTask.append(newBtnDiv);
 
     taskList.append(newTask);
     console.log(taskList)
-});
+}
+
+btnAdd.addEventListener('click', addTask)
 
 btnClear.onclick = () => {
     const tasks = document.querySelectorAll(".task");
